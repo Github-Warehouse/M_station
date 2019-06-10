b79d1d29a90b896a03104a17e38f939b947c208e
sunyang <1802220208@qq.com>
2019-06-10 23:18:05 +0800

    下拉刷新，待完成；页面搭建，待完成；页面滚动。。。

--- src/app.js
+++ src/app.js
@@ -2,5 +2,6 @@ const indexTpl = require('./views/index.html')
 const renderIndexTpl = template.render(indexTpl, {})
 
 import indexController from './controllers/index'
+
 $('#app').html(renderIndexTpl)
 indexController.renderList()
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -6,6 +6,20 @@ const renderList = async () => {
     let data = await fetch.get('/json/result')
     let renderindexListTpl = template.render(indexListTpl, {data})
     $(".m-commodity").html(renderindexListTpl)
+
+    // $(".title").mouseenter(function (e) {
+    //     var thisWidth = $(this).width(); // div 的宽度
+    //     console.log(thisWidth);
+    //     var wordWidth = $(this)[0].scrollWidth; // 先转为js对象; 文字的宽度
+    //     if(wordWidth > thisWidth+5){ // 加5是为了让div宽度多一点,比文字不超出时多宽,因为文字不超出,那么宽度为div的宽度
+    //         $(this).siblings('span.title').html($(this).text()).show();
+    //     }
+        
+    // })
+    // $(".title").mouseleave(function () {
+    //     // layer.msg('显示')
+    //     $(this).siblings('span.title').hide();
+    // })
 }
 
 export default {
